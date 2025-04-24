281390010ae153b1d46855baded31d492ced484d
Kacper Kowalczyk <Kacperkacperkacper987654321@gmail.com>
2025-04-24 19:21:59 +0200

    dodanie metod do obliczania przyjetych obrazen i oblslugi śmierci

--- Character.cs
+++ Character.cs
@@ -7,6 +7,7 @@ public partial class Character : CharacterBody2D
     [Export] public int Health { get; set; }
     [Export] public int AttackDamage { get; set; }
     [Export] public float MovementSpeed { get; set; }
+    // metoda do obliczania movemnetu
     protected void ProcessMovement(Vector2 direction, float delta)
     {
         if (direction == Vector2.Zero)
@@ -20,7 +21,20 @@ public partial class Character : CharacterBody2D
         }
     }
 
-
+    // metoda do obliczania przyjetych obrazen 
+    public virtual void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Death();
+        }
+    }
+    // metoda śmierci
+    public virtual void Death()
+    {
+        QueueFree();
+    }
 
 
 }
